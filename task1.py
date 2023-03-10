61cbd678163f81b60a457b2b261b3b4848157368
NickMoser07 <126705965+NickMoser07@users.noreply.github.com>
2023-03-10 10:25:34 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,9 +1,10 @@
-principle = int(input("please enter your principle"))
-monthlyIrate = int(input("what is your yearly interest rate"))
-numOfmonths = int(input("how many months"))
+principle = int(input("please enter your principle "))
+monthlyIrate = int(input("what is your yearly interest rate "))
+numOfmonths = int(input("how many years"))
 paymentAmount = int(input("how much do you want to add each month"))
 monthlyIrate /= 100
 monthlyIrate /= 12
-
+numOfmonths *= 12
 futureValue = (principle * ( 1 + monthlyIrate) ** numOfmonths) + paymentAmount * ((((1+monthlyIrate)**numOfmonths)-1)/monthlyIrate) * (1+monthlyIrate)    
+print("compounded monthly")
 print(futureValue)
